4ebbe286412c00a6804a316441d4d7d96e03a53c
Vishesh Kushwaha <82991580+vishesh11111@users.noreply.github.com>
2023-06-29 03:06:19 -0700

    Update user.controller.js

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -7,15 +7,8 @@ userController.getUserData = async (req, res) => {
     try {
         let serchText = req.query.search;
         console.log("sddsds=>", serchText)
-        // let query = { firstName: { $regex: `/${serchText}/` } }
-        let userdata = await userModel.aggregate([
-            {
-                $match: { "firstName": serchText }
-            }
-            // {
-            //     $redact : {"firstName": serchText }
-            // }
-        ]);
+        let query = new RegExp(`\\b${serchText}\\w*`, "i")
+        let userdata = await userModel.find({firstName: query});
         res.status(200).send({ sucess: true, data: userdata });
     } catch (error) {
         res.status(500).send({ sucess: false, message: error });
